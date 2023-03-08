606f8a5de13a04845b1bfa4884df21fceeb089b3
dineshwaran-hariram <127334051+dineshwaran-hariram@users.noreply.github.com>
2023-03-08 22:58:10 +0530

    Update streamlit_app.py
    added streamlit.text('Kale, Spinach & Rocket Smoothie')

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,4 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Chicken Sandwich')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
