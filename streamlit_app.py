172950cd38bd1a38f5cedf279444ae5b02eb0d67
dineshwaran-hariram <127334051+dineshwaran-hariram@users.noreply.github.com>
2023-03-11 00:27:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,16 +15,20 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered',fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-fruityvice_normalised = pandas.json_normalize(fruityvice_response.json())
-
-streamlit.dataframe(fruityvice_normalised)
-
 streamlit.header('FRUITYVICE FRUIT ADVICE!')
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit_choice:
+      streamlit.error("Please select a fruit to get information.")
+  else:
+      streamlit.write('The user entered',fruit_choice)
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      fruityvice_normalised = pandas.json_normalize(fruityvice_response.json())
+      streamlit.dataframe(fruityvice_normalised)
+ except URLError as e:
+  streamlit.error()
+
+
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
