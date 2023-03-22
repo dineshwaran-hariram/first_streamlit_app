ee1961bd8942d6e674ffcfa1d9101c960d117196
dineshwaran-hariram <127334051+dineshwaran-hariram@users.noreply.github.com>
2023-03-22 23:21:41 +0530

    Update ZenasStreamLitApp.py

--- ZenasStreamLitApp.py
+++ ZenasStreamLitApp.py
@@ -7,15 +7,15 @@ sl.header("Zena's Amazing Athleisure Catalog")
 
 
 sf_conn=snowflake.connector.connect(**sl.secrets["snowflake"])
-with sf_conn.cursor() as cur:
-  cur.execute("SELECT COLOR_OR_STYLE FROM catalog_for_website")
-  styleList = cur.fetchall()
-#sf_conn.close()
+cur=sf_conn.cursor()
+cur.execute("SELECT COLOR_OR_STYLE FROM catalog_for_website")
+styleList = cur.fetchall()
+
 
 styleListDF=pd.DataFrame(styleList)
 #styleListDF=styleListDF.set_index('COLOR_OR_STYLE')
 
-sl.write(styleListDF)
+#sl.write(styleListDF)
 color_list = styleListDF[0].values.tolist()
 
 styleSelected = sl.selectbox("Pick a sweatsuit color or style:", list(color_list))
