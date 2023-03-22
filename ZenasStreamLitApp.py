a760b8ca0f22d8a7bab4e1710bd93c3a180af96d
dineshwaran-hariram <127334051+dineshwaran-hariram@users.noreply.github.com>
2023-03-22 22:29:33 +0530

    Update ZenasStreamLitApp.py

--- ZenasStreamLitApp.py
+++ ZenasStreamLitApp.py
@@ -4,3 +4,12 @@ import requests as req
 import snowflake.connector as sflake
 
 sl.header("Zena's Amazing Athleisure Catalog")
+
+
+sf_conn=sflake.connect(**streamlit.secrets["snowflake"])
+with sf_conn.cursor() as cur:
+  cur.execute("SELECT * FROM CATALOG")
+catData = cur.fetchall()
+sf_conn.close()
+
+streamlit.dataframe(catData)
