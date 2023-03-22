53c4e4bd650b35ae3b5ffa669c3d365de4440aea
dineshwaran-hariram <127334051+dineshwaran-hariram@users.noreply.github.com>
2023-03-22 23:17:17 +0530

    Update ZenasStreamLitApp.py

--- ZenasStreamLitApp.py
+++ ZenasStreamLitApp.py
@@ -19,7 +19,7 @@ sl.write(styleListDF)
 
 styleSelected = sl.selectbox("Pick a sweatsuit color or style:", list(styleListDF[0]))
 
-product_caption = 'Our warm, comfortable, ' + styleSelected + ' sweatsuit
+product_caption = 'Our warm, comfortable, ' + styleSelected + ' sweatsuit'
 
 #cur.execute("SELECT DIRECT_URL  FROM catalog_for_website WHERE COLOR_OR_STYLE = '"+ styleSelected +"';")
 
